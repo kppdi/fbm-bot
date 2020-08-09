e6071e100a7cfcd55341b6897585e38b3f852810
Buyut Joko Rivai <buyutjokorivai@gmail.com>
2020-08-09 15:30:37 +0800

    Rebuild Routes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(morgan('dev')); // log every request to the console.
 app.use(bodyParser.urlencoded({ extended:false }));
 app.use(bodyParser.json()); 
 // app routes
-// require('./routes/webhook_verify')(app);
+require('./routes/webhook_verify')(app);
 // warming up the engines !! setta !! go !!!.
 app.listen(app.get('port'), function() {
   const url = 'http://localhost:' + app.set('port');
